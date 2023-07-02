ed158d534ed450486589197e3e65572a8480254c
Mahdi Malekan <malekan.59@gmail.com>
2023-07-02 19:03:09 +0200

    s3 upload+cloudfront

--- lib/cdk-s3-upload-stack.ts
+++ lib/cdk-s3-upload-stack.ts
@@ -1,16 +1,35 @@
 import * as cdk from 'aws-cdk-lib';
+import { Distribution, OriginAccessIdentity } from 'aws-cdk-lib/aws-cloudfront';
+import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as path from 'path';
 
 export class CdkS3UploadStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, 'TodoDemoBucket', {
+      accessControl: s3.BucketAccessControl.PRIVATE,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkS3UploadQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new BucketDeployment(this, 'BucketDeployment', {
+      destinationBucket: bucket,
+      sources: [Source.asset(path.resolve(__dirname, '../../vue-demo/dist'))],
+    });
+
+    const originAccessIdentity = new OriginAccessIdentity(
+      this,
+      'OriginAccessIdentity'
+    );
+    bucket.grantRead(originAccessIdentity);
+
+    new Distribution(this, 'Distribution', {
+      defaultRootObject: 'index.html',
+      defaultBehavior: {
+        origin: new S3Origin(bucket, { originAccessIdentity }),
+      },
+    });
   }
 }
